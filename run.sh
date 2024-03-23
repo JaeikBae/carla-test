8e20b1b07b37b413f7a5180b097d413ce6ad1f36
JaeikBae <jaeik@node1.cluster.local>
2024-03-23 19:44:19 +0900

    specify author of docker image

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run --privileged -e SDL_VIDEODRIVER=x11 -e DISPLAY=172.26.165.70:0.0 -v /tmp/.X11-unix:/tmp/.X11-unix -p 2000-2002:2000-2002 --rm -it --gpus=all carla-test bash
+sudo docker run --privileged -e SDL_VIDEODRIVER=x11 -e DISPLAY=172.26.165.70:0.0 -v /tmp/.X11-unix:/tmp/.X11-unix -p 2000-2002:2000-2002 --rm -it --gpus=all jaeik-carla-test bash
